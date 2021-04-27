c9678a0dd92ca88ca1d3c8f3c28c7910fe0758b6
wannadream <wannadream@gmail.com>
2021-04-27 16:44:06 -0600

    updated to angular 11

--- src/app/material-file-upload/material-file-upload.component.spec.ts
+++ src/app/material-file-upload/material-file-upload.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { MaterialFileUploadComponent } from './material-file-upload.component';
 
@@ -6,7 +6,7 @@ describe('MaterialFileUploadComponent', () => {
   let component: MaterialFileUploadComponent;
   let fixture: ComponentFixture<MaterialFileUploadComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MaterialFileUploadComponent ]
     })
